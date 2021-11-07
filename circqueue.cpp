c48fd56ba85cd403a94cf65fd69a1a13e66e9144
Saad Rahman <saadreh5@gmail.com>
2021-11-07 14:40:28 +0500

    implemented the deque not tested yet

--- circqueue.cpp
+++ circqueue.cpp
@@ -33,7 +33,86 @@ class queue
 };
 
 
+
+void queue :: eque(int x)
+{
+    if(front == que  && rear == end)
+    {
+      
+      cout<<"Overflow. \n";
+      return;
+
+    }
+
+    if(front == NULL && rear == NULL)
+    {
+        front = que;
+        rear = que;
+        *rear = x;
+        
+        return;
+
+
+    } 
+
+    if( front!= que  && rear == end )
+    {
+        rear = que;
+        *rear = x;
+        return;
+
+    }
+
+    if(rear + 1 == front)
+    {
+        cout<<"Overflow.\n";
+        return;
+    }
+
+    rear++;
+    *rear = x;
+}
+
+
+void queue:: deque()
+{
+    if(front == NULL && rear == NULL)
+    {
+        cout<<"Underflow.\n";
+        return;
+    }
+
+    if(front == rear )
+    {
+        front = NULL;
+        rear = NULL;
+        return;
+
+    }
+
+    if(front == end)
+    {
+        front = que;
+        return;
+    }
+
+    front++;
+
+}
+
+
 int main()
 {
+    queue obj;
+
+    obj.eque(1);
+    obj.eque(2);
+    obj.eque(3);
+    obj.eque(4);
+    obj.deque();
+    obj.deque();
+    obj.deque();
+    obj.deque();
+    
 
 }
